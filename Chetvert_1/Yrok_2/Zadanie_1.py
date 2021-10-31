50dc541c9ff2bff43d44699e8669fde73ad5089e
tvitalij <tvitalij@yandex.ru>
2021-10-31 11:22:14 +0500

    Zadania_k_Yroky_2

---
+++ Chetvert_1/Yrok_2/Zadanie_1.py
@@ -0,0 +1,21 @@
+# 1. Выяснить тип результата следующих выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+ymnozenie = 15 * 3
+print(ymnozenie)
+print(type(ymnozenie))
+
+delenie = 15 / 3
+print(delenie)
+print(type(delenie))
+
+celaia_chast_ot_delenia =15 // 2
+print(celaia_chast_ot_delenia)
+print(type(celaia_chast_ot_delenia))
+
+vozvedenie_v_stepen = 15 ** 2
+print(vozvedenie_v_stepen)
+print(type(vozvedenie_v_stepen))
